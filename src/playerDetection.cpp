da620574fc748159ab204d6750631036cbd56029
Emanuele999-it <Emanuele.pase99@gmail.com>
2023-09-15 19:05:03 +0200

    detection updated

--- src/playerDetection.cpp
+++ src/playerDetection.cpp
@@ -59,7 +59,7 @@ void playerDetection::startprocess(){
     cv::bilateralFilter(input_clone, blurred, 7, 1000, 400);
     cv::GaussianBlur(blurred,blurred, cv::Size(5,5), 0.5);
 
-    //cv::Mat colorSupp = colorSuppression(blurred, 4);
+    //cv::Mat colorSupp = colorSuppression(blurred, 5);
     output_img = input_image_.clone();
 
     cv::HOGDescriptor hog;
@@ -73,19 +73,24 @@ void playerDetection::startprocess(){
     Mat img_gray;
     cvtColor(blurred, img_gray, cv::COLOR_BGR2GRAY);
 
+    
+
     // Apply local contrast enhancement to the grayscale image using CLAHE
     cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE();
-    clahe->setClipLimit(2); // You can adjust this value to control the enhancement
+    clahe->setClipLimit(4); // You can adjust this value to control the enhancement
     clahe->apply(img_gray, img_gray);
 
-
-    cv::GaussianBlur(img_gray,img_gray, cv::Size(5,5),0.5,3);
+    cv::equalizeHist(img_gray,img_gray);
+    //displayMat(img_gray, "imgray",1);
+    //cv::GaussianBlur(img_gray,img_gray, cv::Size(3,3),0.5,3);
     
     //cv::threshold(img_gray, img_gray, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
 
     int exp = 6;
     int winSizeX = pow(2, exp);
     int winSizeY = pow(2, exp + 1);
+
+    std::vector<cv::Rect> all_rects;
 
     // Ensure that winSizeX and winSizeY are multiples of blockStride
     if (winSizeX <= img_gray.cols && winSizeY <= img_gray.rows) {
@@ -104,22 +109,22 @@ void playerDetection::startprocess(){
             //cv::putText(output_img, label, cv::Point(rects[i].x, rects[i].y - 10), cv::FONT_ITALIC, 0.5, cv::Scalar(0, 255, 0), 2);
 
         }
-*/      
-            for (float scale = 1.0; scale >= 0.5; scale -= 0.1) {
-                cv::Mat resized_img;
-                cv::resize(img_gray, resized_img, cv::Size(), scale, scale);
-                
-                hog.detectMultiScale(resized_img, rects, weights, 0, cv::Size(2, 2), cv::Size(), 1.04, 1, false);
+*/
+        for (float scale = 1.1; scale >= 0.4; scale -= 0.1){
 
-                // Adjust the rectangles to the original image size and draw them
-                for (size_t i = 0; i < rects.size(); i++) {
-                    rects[i] = cv::Rect(rects[i].x / scale, rects[i].y / scale, rects[i].width / scale, rects[i].height / scale);
-                    cv::rectangle(output_img, rects[i], cv::Scalar(0, 255, 0), 2);
-                }
+            cv::Mat resized_img;
+            cv::resize(img_gray, resized_img, cv::Size(), scale, scale);
+
+            hog.detectMultiScale(resized_img, rects, weights, 0, cv::Size(2, 2), cv::Size(15,15), 1.1, 1, false);
+
+            // Adjust the rectangles to the original image size and draw them
+            for (size_t i = 0; i < rects.size(); i++){
+
+                rects[i] = cv::Rect(rects[i].x / scale, rects[i].y / scale, rects[i].width / scale, rects[i].height / scale);
+                cv::rectangle(output_img, rects[i], cv::Scalar(0, 255, 0), 2);
             }
+        }
     }
-
-
 }
 
 
